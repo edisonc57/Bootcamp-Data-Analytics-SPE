08edfa1faa3156930fa6bf8896a8250cacd3e99b
Edison Contreras <79251038+edisonc57@users.noreply.github.com>
2026-08-22 22:58:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,58 @@
-import streamlit as st 
+import streamlit as st
+import pandas as pd
 
-st.title ("Bootcamp-Data-Analytics-SPE")
-st.sidebar.title('Parámetros')
+from funciones_calculos import (calcular_liquido, calcular_bsw, proyectar_produccion)
 
-modulos= st.sidebar.selectbox('Selecciona un módulo', ['Introducción a variables', 'Funciones'])
+from funciones_datos import (filtrar_pozo,resumen_dataframe)
 
-if modulos == 'Introducción a variables':
+from clase_pozo import Pozo
 
-    pozo =  "SPE-001"
-    Petroleo_bpd= 1250
-    agua_bpd = 350.50
-    status = True
+
+st.title("Bootcamp Data Analytics for Oil & Gas")
+st.sidebar.title("Parámetros")
+
+modulos = st.sidebar.selectbox("Selecione un módulo", ["Introducción a variables", "Funciones","POO", "Importación de Librerias"])
+
+if modulos == "Introducción a variables":
+
+  pozo = "SPE-001"
+  petroleo_bpd = 1250
+  agua_bpd = 350.50
+  status = True
+  
+  st.write(pozo)
+  st.write(petroleo_bpd)
+  st.write(agua_bpd)
+  st.write(status)
+elif  modulos == "Funciones": 
+  
+  def calcular_caudal_vogel(caudal_maximo=1000, presion_yacimiento=3000, presion_fondo=200, decimales=2):
+    """
+      Calcula el caudal de petróleo mediante la ecuación de Vogel.
+  
+      Parámetros:
+      caudal_maximo (float): Caudal máximo teórico del pozo, BPD.
+      presion_yacimiento (float): Presión promedio del yacimiento, psi.
+      presion_fondo (float): Presión de fondo fluyente, psi.
+      decimales (int): Número de decimales del resultado.
+  
+      Retorna:
+      float: Caudal estimado de petróleo, BPD.  
+    """
+  
+    relacion_presion = presion_fondo/presion_yacimiento
+    caudal = caudal_maximo*( 1 -0.2*relacion_presion - 0.8*(relacion_presion**2))
     
-    st.write(pozo)
-    st.write(Petroleo_bpd)
-    st.write(agua_bpd)
-    st.write(status)
-elif modulos == 'Funciones':
+    return round(caudal, decimales)
 
-    def calcular_caudal_vogel(caudal_maximo= 1000, presion_yacimiento= 3000, presion_fondo=0, decimales=2):
+  caudal_maximo = st.number_input("Ingrese el caudal máximo", min_value = 0, max_value = 5000, value =1200)
+  presion_yacimiento = st.number_input("Ingrese la presion del yacimiento",min_value = 0, max_value = 9000, value =3000)
+  presion_fondo = st.number_input("Ingrese la presion de fondo fluyente",min_value = 0, max_value = 9000, value =1500)
+  decimales = st.slider("Seleccione la cantidad de decimales para su resultado",min_value = 0, max_value = 4, value =2)
 
-        relacion_presion = presion_fondo / presion_yacimiento
-        caudal = caudal_maximo * (1 - 0.2 * relacion_presion - 0.8 * relacion_presion**2 )
+  caudal = calcular_caudal_vogel(caudal_maximo, presion_yacimiento, presion_fondo, decimales)
 
-        return round(caudal, decimales)
-
-    caudal_maximo= st.number_input ('Ingrese el caudal máximo')
-    presion_yacimiento = st.number_input('Ingrese la presión del yacimiento')
-    presión_fondo = st.number_input('Ingese la presion de fondo fluyente')
-    decimales = st.slider('Seleccione la cantidad de decimales para su resultado')
-    caudal = calcular_caudal_vogel(caudal_maximo, presion_yacimiento, presion_fondo, decimales)
-    
-    st.write("El caudal es:", caudal)
+  st.write("El caudal es:", caudal)
 
 elif  modulos == "POO": 
 
@@ -69,6 +90,5 @@ elif  modulos == "POO":
   st.write(pozo.produccion_total())
 
   dias = st.number_input("Ingrese los días a proyectar", min_value = 0, max_value = 365, value =30)
-  st.write(pozo.proyectar_produccion(dias)) 
-
+  st.write(pozo.proyectar_produccion(dias))
 
