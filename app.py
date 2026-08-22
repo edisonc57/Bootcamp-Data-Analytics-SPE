dba57c8b8d1ba19ffd766741cddb9d2c832197a1
edisonc57 <edisoncontreraspanibra@gmail.com>
2026-08-22 05:34:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 import streamlit as st 
+
+st.title ("Bootcamp-Data-Analytics-SPE")
+
+pozo =  "SPE-001"
+Petroleo_bpd= 1250
+agua_bpd = 350.50
+status = True
+
+st.write(pozo)
+st.write(Petroleo_bpd)
+st.write(agua_bpd)
+st.write(status)
+print()
