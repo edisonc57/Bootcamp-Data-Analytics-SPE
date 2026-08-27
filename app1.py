e25558d4f4d0eafc86f3c37ec6d5dec4eb24497d
edisonc57 <edisoncontreraspanibra@gmail.com>
2026-08-27 03:08:07 +0300

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,15 @@
+import streamlit as st 
+
+st.title ("Bootcamp-Data-Analytics-SPE")
+
+pozo =  "SPE-001"
+Petroleo_bpd= 1250
+agua_bpd = 350.50
+status = True
+
+st.write(pozo)
+st.write(Petroleo_bpd)
+st.write(agua_bpd)
+st.write(status)
+
+st.sidebar.title('Parámetros')
