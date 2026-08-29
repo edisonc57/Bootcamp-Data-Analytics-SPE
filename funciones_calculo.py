8f9c95a1c29917f5f7fe3cf87fc63b6d35fe95cd
edisonc57 <edisoncontreraspanibra@gmail.com>
2026-08-29 18:22:49 +0300

    Create funciones_calculo.py

---
+++ funciones_calculo.py
@@ -0,0 +1,16 @@
+def calcular_liquido(petroleo, agua):
+    return petroleo + agua
+
+def calcular_bsw(petroleo,agua):
+    liquido = calcular_liquido(petroleo,agua)
+    if liquido == 0:
+        return 0
+    return (agua /liquido) * 100
+
+def calcular_gor(gas, petroleo):
+    if petroleo == 0:
+        return  0
+    return (gas / petroleo) *1000
+
+def proyectar_produccion (produccion_diaria, dias):
+    return produccion_diaria * dias
